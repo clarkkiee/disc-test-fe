0c7ba9d04cfe5be64a1ac1ccccb74dac3f15ae22
clarkkkkk <juanfarrel0404@gmail.com>
2024-07-07 06:24:03 +0700

    feat

--- src/services/UserActionServices.ts
+++ src/services/UserActionServices.ts
@@ -1,11 +1,34 @@
+"use server"
+import { cookies } from "next/headers";
 import apiClient from "./apiClient";
+import { getAuthToken } from "./AuthTokenService";
 
 export async function registerUser( userData : any ){
     try {
         const response = await apiClient.post('/user/register', userData);
-        console.log(response)
+        const token = response.data.data.authorization
+        cookies().set('access_token', token)
+        return response.data.data
     } catch (error) {
         console.error(error);
         return null
+    }
+}
+
+export async function getCurrentUser(){
+    try {
+        const token = await getAuthToken()
+        if(!token){
+            return undefined
+        }
+        const currentUser = await apiClient.get('/user/current', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        return currentUser.data.data
+    } catch (error) {
+        console.error(error);
+        return undefined;
     }
 }
